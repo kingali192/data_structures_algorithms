a47880be03eea661ada197050ab4634719174b68
kingali192 <mali8715@gmail.com>
2020-01-01 16:34:54 -0800

    Update binary_search_tree.py

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -45,5 +45,60 @@ class Node(object):
             res.append(root.val)
         return res
     
+    def inorder_iterative(self, root):
+        if not root:
+            return
+        
+        stack = []
+        node = root
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                result.append(node.value)
+                node = node.right
+    
+    def preorder_iterative(self, root):
+        if not root: 
+            return
+        
+        stack = []
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            result.append(node.value)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+            
+    
+    def postorder_iterative(self, root):
+        if not root:
+            return    
+        visited = set()
+        stack = []
+        node = root
+        
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                
+                if node.right and not node.right in visited:
+                    stack.append(node)
+                    node = node.right
+                else:
+                    visited.add(node)
+                    result.append(node.value)
+                    node = None
+    
+    
+    
+    
     
             
